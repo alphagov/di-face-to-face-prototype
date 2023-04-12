a92c6d4624027397a233510c91780ef6ceae60f6
quarshie-lauren <115984791+quarshie-lauren@users.noreply.github.com>
2023-04-12 22:26:56 +0100

    Add files via upload

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const router = express.Router()
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
 
-//radio redirect package https://github.com/abbott567/radio-button-redirect
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// Add your routes here
+
 const radioButtonRedirect = require('radio-button-redirect')
 router.use(radioButtonRedirect)
-
-// Add your routes here - above the module.exports line
-module.exports = router
 
 
 
